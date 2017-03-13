3d1b2a7a3b67ffd546e4b960530c2c594cd02411
Jeanine Bonot <jeanine.bonot@rwth-aachen.de>
2017-03-13 22:51:31 +0100

    To debug: Import dummy sensor.

--- accelerometerApp/src/pages/home/home.ts
+++ accelerometerApp/src/pages/home/home.ts
@@ -4,6 +4,8 @@ import { NavController } from 'ionic-angular';
 import { DeviceMotion, DeviceMotionAccelerationData } from 'ionic-native';
 import { Subscription }   from 'rxjs/Subscription';
 
+import { DummySensor } from 'dummy-sensor';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,11 +13,16 @@ import { Subscription }   from 'rxjs/Subscription';
 
 export class HomePage {
   private subscription: Subscription;
+  private sensor: DummySensor;
+  private name: string = 'Mobile Accelerometer';
   value: string;
   toggle: boolean = false;
 
   constructor(public navCtrl: NavController) {
-
+    this.sensor = new DummySensor(3500, {frequency: 1000});
+    this.sensor.onchange = event => this.sensor.reading = event.reading;
+    this.sensor.name = this.name;
+    this.sensor.formula = () => { return +this.value };
   }
 
   toggleRead() {
@@ -23,8 +30,10 @@ export class HomePage {
       this.subscription = DeviceMotion.watchAcceleration({ frequency: 1000 }).subscribe(
         (acceleration: DeviceMotionAccelerationData) => {
           this.value = acceleration.x.toString();
+          this.sensor.start();
         });
     } else {
+      this.sensor.stop();
       this.subscription.unsubscribe();
       this.value = '';
     }
