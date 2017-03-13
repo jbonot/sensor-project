453532d5d38a02171042185ef9120bff775ea45a
Jeanine Bonot <jeanine.bonot@rwth-aachen.de>
2017-03-13 21:46:43 +0100

    Toggle read accelerometer data.

--- accelerometerApp/src/pages/home/home.ts
+++ accelerometerApp/src/pages/home/home.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+
+import { DeviceMotion, DeviceMotionAccelerationData } from 'ionic-native';
+import { Subscription }   from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  private subscription: Subscription;
+  value: string;
+  toggle: boolean = false;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  toggleRead() {
+    if (this.toggle) {
+      this.subscription = DeviceMotion.watchAcceleration({ frequency: 1000 }).subscribe(
+        (acceleration: DeviceMotionAccelerationData) => {
+          this.value = acceleration.x.toString();
+        });
+    } else {
+      this.subscription.unsubscribe();
+      this.value = '';
+    }
+  }
 }
